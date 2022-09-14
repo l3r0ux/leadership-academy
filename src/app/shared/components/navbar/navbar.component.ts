36b6b021fb156ed6fd1d6983977fbb39c0ed676f
Le Roux <53217770+l3r0ux@users.noreply.github.com>
2022-09-14 17:04:11 +0200

    add user profile avatar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild('details') details!: ElementRef
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  openDetails(): void {
+    this.details.nativeElement.classList.toggle('visible')
+  }
 }
