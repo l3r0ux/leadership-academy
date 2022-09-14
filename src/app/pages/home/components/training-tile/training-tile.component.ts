2deaaee566e2b76f645d37dbe0758ca6f90e6ae6
Le Roux <53217770+l3r0ux@users.noreply.github.com>
2022-09-14 16:38:47 +0200

    add routing for home page tiles

--- src/app/pages/home/components/training-tile/training-tile.component.ts
+++ src/app/pages/home/components/training-tile/training-tile.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-training-tile',
@@ -9,8 +10,19 @@ export class TrainingTileComponent implements OnInit {
   @Input() tile!: any;
   @ViewChild('image', {static: true}) image!: ElementRef;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    console.log(this.tile)
+  }
+
+  navigate(): void {
+    if (this.tile.text.toLowerCase().includes('academy')) {
+      this.router.navigate(['/leadership-academy'])
+    } else if (this.tile.text.toLowerCase().includes('forum')) {
+      this.router.navigate(['/the-forum'])
+    } else if (this.tile.text.toLowerCase().includes('pauline')) {
+      this.router.navigate(['/pauline-leadership'])
+    }
   }
 }
