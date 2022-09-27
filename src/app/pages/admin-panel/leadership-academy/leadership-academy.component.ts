c2b8036d3fd660b7e2ba4649a7dd1d71b79ce847
Le Roux <53217770+l3r0ux@users.noreply.github.com>
2022-09-27 17:48:01 +0200

    add virtual sessions to leadership-academy

--- src/app/pages/admin-panel/leadership-academy/leadership-academy.component.ts
+++ src/app/pages/admin-panel/leadership-academy/leadership-academy.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Conference, Session } from 'src/app/shared/models/conference';
+import { conferences, sessions } from 'src/app/shared/dummyData';
 
 @Component({
   selector: 'app-leadership-academy',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leadership-academy.component.scss']
 })
 export class LeadershipAcademyAdminComponent implements OnInit {
+  tabSelected: any = {
+    title: 'Conferences',
+    selector: 'conferences',
+    routerLink: undefined
+  }
+  conferences: Array<Conference> = conferences
+  sessions: Array<Session> = sessions
+  tabs: Array<any> = [
+    {
+      title: 'Conferences',
+      selector: 'conferences',
+      routerLink: undefined
+    },
+    {
+      title: 'Virtual sessions',
+      selector: 'sessions',
+      routerLink: undefined
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setTab(tab: any): void {
+    this.tabSelected = tab
+  }
 }
--- src/app/pages/leadership-academy/leadership-academy.component.ts
+++ src/app/pages/leadership-academy/leadership-academy.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Conference } from 'src/app/shared/models/conference';
-import { conferences } from 'src/app/shared/dummyData';
+import { conferences, sessions } from 'src/app/shared/dummyData';
+import { Session } from 'src/app/shared/models/session';
 
 @Component({
   selector: 'app-leadership-academy',
@@ -9,9 +10,31 @@ import { conferences } from 'src/app/shared/dummyData';
 })
 export class LeadershipAcademyComponent implements OnInit {
   conferences: Array<Conference> = conferences;
+  sessions: Array<Session> = sessions;
+  tabSelected: any = {
+    title: 'Conferences',
+    selector: 'conferences',
+    routerLink: undefined
+  }
+  tabs: Array<any> = [
+    {
+      title: 'Conferences',
+      selector: 'conferences',
+      routerLink: undefined
+    },
+    {
+      title: 'Virtual sessions',
+      selector: 'sessions',
+      routerLink: undefined
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setTab(tab: any): void {
+    this.tabSelected = tab
   }
 }
