b87c16d813d46e8dde3287ddd7d6fee5c1eaf3da
Le Roux <53217770+l3r0ux@users.noreply.github.com>
2022-09-27 18:57:17 +0200

    register admin modals on shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -16,6 +16,11 @@ import { TabsComponent } from './components/tabs/tabs.component';
 import { ApplicationComponent } from './components/admin/application/application.component';
 import { SessionsAdminComponent } from './components/admin/sessions/sessions.component';
 import { ConferencesAdminComponent } from './components/admin/conferences/conferences.component';
+import { AddConferenceComponent } from './components/admin/modals/add-conference/add-conference.component';
+import { AddImagesComponent } from './components/admin/modals/add-images/add-images.component';
+import { AddSessionComponent } from './components/admin/modals/add-session/add-session.component';
+import { AddTeachingMaterialComponent } from './components/admin/modals/add-teaching-material/add-teaching-material.component';
+import { AddVideoComponent } from './components/admin/modals/add-video/add-video.component';
 
 const components = [
   TabsComponent,
@@ -32,7 +37,12 @@ const components = [
   ApplyComponent,
   ApplicationComponent,
   SessionsAdminComponent,
-  ConferencesAdminComponent
+  ConferencesAdminComponent,
+  AddConferenceComponent,
+  AddImagesComponent,
+  AddSessionComponent,
+  AddTeachingMaterialComponent,
+  AddVideoComponent,
 ]
 
 @NgModule({
